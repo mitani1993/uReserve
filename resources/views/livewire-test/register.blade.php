ee3d5a2862a9b558df3a272128c82e9015f13cd8
mitani1993 <fytim55@gmail.com>
2023-01-26 16:33:33 +0900

    tailwind読み込み

--- resources/views/livewire-test/register.blade.php
+++ resources/views/livewire-test/register.blade.php
@@ -1,9 +1,13 @@
 <html>
     <head>
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        <!-- Styles -->
         @livewireStyles
     </head>
     <body>
-        Livewireテスト
+        Livewireテスト <span class="text-blue-600">register</span>
         @livewire('register')
         @livewireScripts
     </body>
