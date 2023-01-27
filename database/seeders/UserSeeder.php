a97b31e727d41da8bbdea1fd2eba0323fd3b2315
mitani1993 <fytim55@gmail.com>
2023-01-27 17:13:57 +0900

    roleの追加

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -14,6 +16,25 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('users')->insert([
+            [
+                'name' => 'admin',
+                'email' => 'admin@admin.com',
+                'password' => Hash::make('password123'),
+                'role' => 1
+            ],
+            [
+                'name' => 'manager',
+                'email' => 'manager@manager.com',
+                'password' => Hash::make('password123'),
+                'role' => 5
+            ],
+            [
+                'name' => 'test',
+                'email' => 'test@test.com',
+                'password' => Hash::make('password123'),
+                'role' => 9
+            ],
+        ]);
     }
 }
