39a0fbb7696cab8b61a04fd41d9f88a81b5626ff
mitani1993 <fytim55@gmail.com>
2023-01-28 17:00:30 +0900

    イベント詳細ページ

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -70,15 +70,17 @@ class EventController extends Controller
         return to_route('events.index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
     public function show(Event $event)
     {
-        //
+        $event = Event::findOrFail($event->id);
+        $eventDate = $event->eventDate;
+        $startTime = $event->startTime;
+        $endTime = $event->endTime;
+
+        return view(
+            'manager.events.show',
+            compact('event', 'eventDate', 'startTime', 'endTime')
+        );
     }
 
     /**
