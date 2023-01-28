eea6d8c509e20e12fca4af64527b2eb77e53ef4c
mitani1993 <fytim55@gmail.com>
2023-01-28 17:27:02 +0900

    更新処理

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -96,16 +96,41 @@ class EventController extends Controller
         );
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateEventRequest  $request
-     * @param  \App\Models\Event  $event
-     * @return \Illuminate\Http\Response
-     */
     public function update(UpdateEventRequest $request, Event $event)
     {
-        //
+        $check = EventService::countEventDuplication(
+            $request['event_date'],
+            $request['start_time'],
+            $request['end_time']
+        );
+
+        if ($check > 1) {
+            $event = Event::findOrFail($event->id);
+            $eventDate = $event->editEventDate;
+            $startTime = $event->startTime;
+            $endTime = $event->endTime;
+            session()->flash('status', 'この時間帯は既に他の予約が存在します。');
+            return view(
+                'manager.events.edit',
+                compact('event', 'eventDate', 'startTime', 'endTime')
+            );
+        }
+
+        $startDate = EventService::joinDateAndTime($request['event_date'], $request['start_time']);
+        $endDate = EventService::joinDateAndTime($request['event_date'], $request['end_time']);
+
+        $event = Event::findOrFail($event->id);
+        $event->name = $request['event_name'];
+        $event->information = $request['information'];
+        $event->start_date =  $startDate;
+        $event->end_date = $endDate;
+        $event->max_people = $request['max_people'];
+        $event->is_visible = $request['is_visible'];
+        $event->save();
+
+        session()->flash('status', '更新しました。');
+
+        return to_route('events.index');
     }
 
     /**
