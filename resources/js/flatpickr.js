e508dcca361a93964c405f15bb23a33cc44671f5
mitani1993 <fytim55@gmail.com>
2023-01-31 12:59:10 +0900

    予約カレンダーの準備

--- resources/js/flatpickr.js
+++ resources/js/flatpickr.js
@@ -8,6 +8,12 @@ flatpickr("#event_date", {
     maxDate: new Date().fp_incr(30)
 });
 
+flatpickr("#calendar", {
+    "locale": Japanese,
+    minDate: "today",
+    maxDate: new Date().fp_incr(30)
+});
+
 // 時間表示、カレンダー非表示、24時間表記
 const setting = {
     "locale": Japanese,
@@ -17,6 +23,7 @@ const setting = {
     time_24hr: true,
     minTime: "10:00",
     maxTime: "20:00",
+    minuteIncrement: 30,
 }
 
 flatpickr("#start_time", setting);
