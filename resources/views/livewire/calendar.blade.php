4104f459a4a3fb9b23c1ae839f61f38a18069f17
mitani1993 <fytim55@gmail.com>
2023-01-31 15:04:01 +0900

    Calendarレイアウト

--- resources/views/livewire/calendar.blade.php
+++ resources/views/livewire/calendar.blade.php
@@ -3,7 +3,6 @@
     <input id="calendar" class="block mt-1 w-full" type="text" name="calendar"
         value="{{ $currentDate }}" wire:change="getDate($event.target.value)"
     />
-    {{ $currentDate }}
     <div class="flex">
         @for($day = 0; $day < 7; $day++)
             {{ $currentWeek[$day] }}
