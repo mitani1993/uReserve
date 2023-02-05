91f3e3b0cc2e43d58bfd9f5c51d3c79c418cf566
mitani1993 <fytim55@gmail.com>
2023-02-05 17:22:13 +0900

    dashboardのルーティングを変更

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class ReservationController extends Controller
 {
-    //
+    public function dashboard()
+    {
+        return view('dashboard');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AlpineTestController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\LivewireTestController;
+use App\Http\Controllers\ReservationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,28 +21,24 @@ Route::get('/', function () {
     return view('calendar');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+// Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
+
+
 
 Route::prefix('manager')
     ->middleware('can:manager-higher')
     ->group(function () {
-        Route::get('index', function () {
-            dd('manager');
-        });
+        Route::get('events/past', [EventController::class, 'past'])->name('events.past');
+        Route::resource('events', EventController::class);
     });
 
 Route::middleware('can:user-higher')
     ->group(function () {
-        Route::get('events/past', [EventController::class, 'past'])->name('events.past');
-        Route::resource('events', EventController::class);
+        Route::get('/dashboard', [ReservationController::class, 'dashboard'])->name('dashboard');
     });
 
 Route::controller(LivewireTestController::class)
