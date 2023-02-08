6285ee5ad5555a8ec26c6d66ddccfa7e599b7c35
mitani1993 <fytim55@gmail.com>
2023-02-08 15:18:32 +0900

    未ログインでイベント詳細も表示する方法

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,7 @@ Route::middleware('can:user-higher')
         // Route::get('/{id}', [ReservationController::class, 'detail'])->name('events.detail');
         Route::post('/{id}', [ReservationController::class, 'reserve'])->name('events.reserve');
     });
-Route::middleware('auth')->get('/{id}', [ReservationController::class, 'detail'])->name('events.detail');
+Route::get('/{id}', [ReservationController::class, 'detail'])->name('events.detail');
 
 Route::controller(LivewireTestController::class)
     ->prefix('livewire-test')->name('livewire-test.')->group(function () {
