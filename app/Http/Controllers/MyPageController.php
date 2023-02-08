6134de0988b957f305e46a5921d37ca90b6210d4
mitani1993 <fytim55@gmail.com>
2023-02-08 14:24:48 +0900

    マイページの準備、コントローラ

--- app/Http/Controllers/MyPageController.php
+++ app/Http/Controllers/MyPageController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Services\MyPageService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class MyPageController extends Controller
 {
-    //
+    public function index()
+    {
+        $user = User::findOrFail(Auth::id());
+        $events = $user->events;
+        $fromTodayEvents = MyPageService::reservedEvent($events, 'fromToday');
+        $pastEvents = MyPageService::reservedEvent($events, 'past');
+
+        return view(
+            'mypage/index',
+            compact('fromTodayEvents', 'pastEvents')
+        );
+    }
 }
